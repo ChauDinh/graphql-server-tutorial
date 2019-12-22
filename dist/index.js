a092d407a36ef6b8eeb0a976945c5bd3c62a91e9
Chau Dinh <chaudvb95uc@gmail.com>
2019-12-22 13:46:20 +0700

    apollo-server: type def and resolvers

--- dist/index.js
+++ dist/index.js
@@ -2,12 +2,14 @@
 
 var _express = _interopRequireDefault(require("express"));
 
+var _cors = _interopRequireDefault(require("cors"));
+
 var _apolloServerExpress = require("apollo-server-express");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 function _templateObject() {
-  var data = _taggedTemplateLiteral(["\n  type Query {\n    me: User\n  }\n\n  type User {\n    username: String!\n  }\n"]);
+  var data = _taggedTemplateLiteral(["\n  type Query {\n    me: User\n    user(id: ID!): User\n    users: [User!]\n  }\n\n  type User {\n    id: ID!\n    username: String!\n  }\n"]);
 
   _templateObject = function _templateObject() {
     return data;
@@ -18,14 +20,41 @@ function _templateObject() {
 
 function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
 
+var _users = {
+  1: {
+    id: "1",
+    username: "Jascha Heifetz"
+  },
+  2: {
+    id: "2",
+    username: "David Oistrakh"
+  }
+};
+var _me = _users[1];
 var app = (0, _express["default"])();
+app.use((0, _cors["default"])());
+/**
+ * GraphQL Schema provides to the Apollo server all data for reading and writing via graphql.
+ * The schema consists type definitions
+ */
+
 var schema = (0, _apolloServerExpress.gql)(_templateObject());
+/**
+ * Resolvers are used to return data for fields from the schema.
+ * Resolver is a function that resolves data for graphql fields in the schema.
+ */
+
 var resolvers = {
   Query: {
     me: function me() {
-      return {
-        username: "Chau Dinh"
-      };
+      return _me;
+    },
+    user: function user(parent, _ref) {
+      var id = _ref.id;
+      return _users[id];
+    },
+    users: function users() {
+      return Object.values(_users);
     }
   }
 };
--- src/index.js
+++ src/index.js
@@ -2,26 +2,54 @@ import express from "express";
 import cors from "cors";
 import { ApolloServer, gql } from "apollo-server-express";
 
+let users = {
+  1: {
+    id: "1",
+    username: "Jascha Heifetz"
+  },
+  2: {
+    id: "2",
+    username: "David Oistrakh"
+  }
+};
+
+const me = users[1];
+
 const app = express();
 
 app.use(cors());
 
+/**
+ * GraphQL Schema provides to the Apollo server all data for reading and writing via graphql.
+ * The schema consists type definitions
+ */
 const schema = gql`
   type Query {
     me: User
+    user(id: ID!): User
+    users: [User!]
   }
 
   type User {
+    id: ID!
     username: String!
   }
 `;
 
+/**
+ * Resolvers are used to return data for fields from the schema.
+ * Resolver is a function that resolves data for graphql fields in the schema.
+ */
 const resolvers = {
   Query: {
     me: () => {
-      return {
-        username: "Chau Dinh"
-      };
+      return me;
+    },
+    user: (parent, { id }) => {
+      return users[id];
+    },
+    users: () => {
+      return Object.values(users);
     }
   }
 };
