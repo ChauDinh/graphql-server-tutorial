ce6f9712c1abadcff8bc29342e58de619528ee23
Chau Dinh <chaudvb95uc@gmail.com>
2019-12-25 20:10:07 +0700

    domain separation

---
+++ src/schema/user.js
@@ -0,0 +1,16 @@
+import { gql } from "apollo-server-express";
+
+export default gql`
+  extend type Query {
+    me: User
+    user(id: ID!): User
+    users: [User!]
+    onlineUsers: [User!]!
+  }
+
+  type User {
+    id: ID!
+    username: String!
+    messages: [Message!]
+  }
+`;
